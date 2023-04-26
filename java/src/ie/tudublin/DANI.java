de091d4581fb0fcf8131fea7588182332dec84cd
naomitogbe <togbenaomi56@gmail.com>
2023-04-26 18:53:36 +0100

    p3

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -11,6 +11,7 @@ public class DANI extends PApplet {
 		//fullScreen(SPAN);
 	}
 
+
     String[] sonnet;
 
     public String[] writeSonnet()
@@ -24,9 +25,43 @@ public class DANI extends PApplet {
        
 	}
 
+	// writes sonnet when spacebar is pressed
 	public void keyPressed() {
-
+		if (keyCode == ' ')
+		{
+			writeSonnet();
+		}
+	
 	}
+
+	// loads the document into the module
+	void loadFile() {
+		String[] lines = loadStrings("small.txt"); // load a text file into a string array
+		for (String line : lines) { 
+			String[] words = split(line, ' '); // splits a string into an array of words
+			for (String w : words) {
+				w.replaceAll("[^\\w\\s]", ""); //removes puncuation
+				w.toLowerCase(); //converts a string to lower case
+			}
+		}
+	}
+
+	// finds a word in model that checks if it exists
+	public void findWord(String str) {
+		for (String line : sonnet) {
+			if (line.contains(str)) {
+				System.out.println();
+			}
+		}
+	}
+
+	// prints the text in the text file
+	public void printModel() {
+		for (String line : sonnet) {
+			System.out.println(line);
+		}
+	}
+
 
 	float off = 0;
 
